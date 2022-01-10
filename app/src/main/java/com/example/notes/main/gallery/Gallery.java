cd114b74214167e7bdd8c7bbf3db6357fd0126ca
Brodzio <bartek.brodawka@gmail.com>
2022-01-10 13:50:13 +0100

    Poprawione usuwanie zdjec i obrazow

--- app/src/main/java/com/example/notes/main/gallery/Gallery.java
+++ app/src/main/java/com/example/notes/main/gallery/Gallery.java
@@ -48,10 +48,6 @@ public class Gallery extends AppCompatActivity implements ImageAdapter.OnItemCli
     private List<Upload> mUploads;
     private FirebaseUser firebaseUser;
 
-
-    //
-//    FirebaseRecyclerAdapter<Member, ViewHolder> firebaseRecyclerAdapter;
-//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -108,8 +104,9 @@ public class Gallery extends AppCompatActivity implements ImageAdapter.OnItemCli
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 GroupModel model = snapshot.getValue(GroupModel.class);
-                System.out.println("Numer obrazka id "+ Integer.parseInt(selectedKey));
-                model.removeNoteFromList(Integer.parseInt(selectedKey)); //Nie usuwa nic <----------------------------------------------------------------------------- tutaj problem
+                System.out.println("Numer obrazka id " + Integer.parseInt(selectedKey));
+                model.removeNoteFromList(Integer.parseInt(selectedKey));
+                databaseReference.child(firebaseUser.getUid()).child("groups").child(groupName).setValue(model);
                 finish();
                 startActivity(getIntent());
             }
@@ -119,71 +116,7 @@ public class Gallery extends AppCompatActivity implements ImageAdapter.OnItemCli
 
             }
         });
-
-//        databaseReference.child(firebaseUser.getUid()).child("groups").child(groupName).child("notes").child(selectedKey).removeValue();
-
-
-        //Toast.makeText(this, "Delete click at position: " + position, Toast.LENGTH_SHORT).show();
     }
-//
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//
-//
-//
-//        Query query = databaseReference.child(firebaseUser.getUid()).child("groups").child(groupName).child("notes").orderByChild("type").equalTo(NoteType.PAINT.name());
-//
-//        FirebaseRecyclerOptions<Member> options = new FirebaseRecyclerOptions.Builder<Member>()
-//                .setQuery(query, Member.class)
-//                .build();
-//
-//        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Member, ViewHolder>(options) {
-//            @Override
-//            protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull Member model) {
-//                holder.setDetails(getApplication(), model.getType(), model.getContent());
-//            }
-//
-//            @NonNull
-//            @Override
-//            public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//                View rootView = LayoutInflater.from(parent.getContext()).inflate(R.layout.image, parent, false);
-//
-//                ViewHolder viewHolder = new ViewHolder(rootView);
-//
-//                viewHolder.setOnClickListener(new ViewHolder.ClickListener() {
-//                    @Override
-//                    public void onItemClick(View view, int position) {
-//
-//                        ImageView imageView = (ImageView) view.findViewById(R.id.rImgaeView);
-//
-//                        Drawable drawable = imageView.getDrawable();
-//                        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
-//
-//                        Intent intent = new Intent(view.getContext(), SingleImage.class);
-//                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-//                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-//                        byte[]bytes = stream.toByteArray();
-//
-//                        intent.putExtra("image", bytes);
-//                        startActivity(intent);
-//
-//                    }
-//
-//                    @Override
-//                    public void onItemLongClick(View view, int position) {
-//                        // own implementation
-//                    }
-//                });
-//
-//                return viewHolder;
-//            }
-//        };
-//
-//        mRecyclerView.setAdapter(firebaseRecyclerAdapter);
-//
-//        firebaseRecyclerAdapter.startListening();
-//    }
 
     private void extractExtras() {
         Bundle extras = getIntent().getExtras();
